8d650820679e53cb85e3e578d54e320680a0cb91
kartikk221 <kartikk221@yahoo.com>
2022-05-18 19:50:36 -0400

    Fixed Types

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,9 +3,9 @@ type SupportedTypes = string | number | boolean;
 type SupportedArgumentTypes = SupportedTypes | SupportedTypes[];
 
 interface ValueRecord {
-    value: T,
+    value: InstanceType<typeof CachedLookup>,
     updated_at: number
-};
+}
 
 export default class CachedLookup<T extends any> {
     /**
@@ -24,7 +24,7 @@ export default class CachedLookup<T extends any> {
      * @param {...(SupportedArgumentTypes)} args
      * @returns {Promise<T>}
      */
-    cached(max_age: number, ...args: SupportedArgumentTypes): Promise<T>;
+    cached(max_age: number, ...args: SupportedArgumentTypes[]): Promise<T>;
 
     /**
      * Returns a fresh value and automatically updates the internal cache with this value for the provided set of arguments.
@@ -32,7 +32,7 @@ export default class CachedLookup<T extends any> {
      * @param {...(SupportedArgumentTypes)} args
      * @returns {Promise<T>}
      */
-    fresh(...args: SupportedArgumentTypes): Promise<T>;
+    fresh(...args: SupportedArgumentTypes[]): Promise<T>;
 
     /**
      * Expires the cached value for the provided set of arguments.
@@ -40,7 +40,7 @@ export default class CachedLookup<T extends any> {
      * @param {...(SupportedArgumentTypes)} args
      * @returns {Boolean} True if the cache value was expired, false otherwise.
      */
-    expire(...args: SupportedArgumentTypes): bool;
+    expire(...args: SupportedArgumentTypes[]): boolean;
 
     /**
      * Returns whether a fresh value is currently being resolved for the provided set of arguments.
@@ -48,7 +48,7 @@ export default class CachedLookup<T extends any> {
      * @param {...(SupportedArgumentTypes)} args
      * @returns {Boolean}
      */
-    in_flight(...args: SupportedArgumentTypes): bool;
+    in_flight(...args: SupportedArgumentTypes[]): boolean;
 
     /**
      * Returns the last value update timestamp in milliseconds for the provided set of arguments.
@@ -56,7 +56,7 @@ export default class CachedLookup<T extends any> {
      * @param {...(SupportedArgumentTypes)} args
      * @returns {Boolean}
      */
-    updated_at(...args: SupportedArgumentTypes): number | void;
+    updated_at(...args: SupportedArgumentTypes[]): number | void;
 
     /* CachedLookup Getters */
 
